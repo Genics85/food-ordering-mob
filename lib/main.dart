9e40d9b5cdf2511a9809f928129b6b38ec6f5cad
unknown <eugeneamo85@gmail.com>
2022-01-30 23:04:40 +0000

    home page done with only clippath left

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:food/pages/homePage.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return(
+    MaterialApp(
+      title: "Food App",
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+    )
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // void main() {
 //   runApp(const MyApp());
